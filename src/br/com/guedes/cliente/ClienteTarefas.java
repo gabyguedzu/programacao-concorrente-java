f7cd55c9573426b960c043a829969a3779fa977d
Gabriela Guedes <zooiv3rde@gmail.com>
2020-04-30 19:11:41 -0300

    testando conecao com socket

--- src/br/com/guedes/cliente/ClienteTarefas.java
+++ src/br/com/guedes/cliente/ClienteTarefas.java
@@ -1,5 +1,16 @@
 package br.com.guedes.cliente;
 
-public class ClienteTarefas {
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
+public class ClienteTarefas {
+	
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		Socket socket = new Socket("localhost", 12345);
+		
+		System.out.println("conexao estabelecida");
+		
+		socket.close();
+	}
 }
