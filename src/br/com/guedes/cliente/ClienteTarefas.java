a703e533b8d425545eff2397c00afd7022438a97
Gabriela Guedes <zooiv3rde@gmail.com>
2020-05-09 18:54:46 -0300

    thread join

--- src/br/com/guedes/cliente/ClienteTarefas.java
+++ src/br/com/guedes/cliente/ClienteTarefas.java
@@ -7,21 +7,59 @@ import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class ClienteTarefas {
-	
-	public static void main(String[] args) throws UnknownHostException, IOException {
+
+	public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException {
 		Socket socket = new Socket("localhost", 12345);
+
+		System.out.println("conexão estabelecida");
+
+		Thread threadEnviaComando = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					PrintStream saida = new PrintStream(socket.getOutputStream());
+					Scanner entrada = new Scanner(System.in);
+					while (entrada.hasNextLine()) {
+						String nextLine = entrada.nextLine();
+
+						if (nextLine.trim().equals("")) {
+							break;
+						}
+						saida.println(nextLine);
+					}
+					saida.close();
+					entrada.close();
+				} catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+			}
+		});
+
+		Thread threadRecebeRespostaDoServidor = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					Scanner entradaServidor = new Scanner(socket.getInputStream());
+					while (entradaServidor.hasNextLine()) {
+						String nextLineServer = entradaServidor.nextLine();
+						System.out.println(nextLineServer);
+					}
+					entradaServidor.close();
+				} catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+			}
+		});
 		
-		System.out.println("conexao estabelecida");
+		threadEnviaComando.start();
+		threadRecebeRespostaDoServidor.start();
 		
-		Scanner entrada = new Scanner(System.in);
+		threadEnviaComando.join();
 		
-		PrintStream saida = new PrintStream(socket.getOutputStream());
-		saida.println("c1");
+		System.out.println("Fechando socket do cliente");
 		
-		entrada.nextLine();
-		
-		saida.close();
-		entrada.close();
 		socket.close();
 	}
 }
